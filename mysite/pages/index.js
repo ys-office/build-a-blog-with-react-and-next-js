c4ee9e5d63ff4b0003fa5d74daeb2d348a046033
Yusuke Sato <yusuke.sato@ys-office.me>
2021-02-11 22:39:00 +0900

    Adding HTML <head> Elements

--- mysite/pages/index.js
+++ mysite/pages/index.js
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>Next.js starter site</title>
+                <meta
+                    name="description"
+                    content="An example Next.js site."
+                />
+            </Head>
+
             <h1>Next.js starter site</h1>
             <p>This is a demonstration blog using <a href="https://nextjs.org/">Next.js</a>.</p>
             <p>Find out more <Link href="/about"><a>about us...</a></Link></p>
