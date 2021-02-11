60e1e2c13b7178c1181ca69be24de77c88e1d604
Yusuke Sato <yusuke.sato@ys-office.me>
2021-02-11 20:00:47 +0900

    create similar files in the pages directory which are automatically routed

--- mysite/pages/index.js
+++ mysite/pages/index.js
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
     return (
         <>
             <h1>Next.js starter site</h1>
             <p>This is a demonstration blog using <a href="https://nextjs.org/">Next.js</a>.</p>
+            <p>Find out more <Link href="/about"><a>about us...</a></Link></p>
+            <p>Find out more <Link href="/contact"><a>contact us...</a></Link></p>
         </>
     );
 }
